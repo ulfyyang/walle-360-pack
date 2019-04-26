abf33bb19deafb99b1d67f87c19b2340362f228c
spark <ulfyyang@gmail.com>
2019-04-26 20:10:28 +0800

    feat: 初始提交

--- dabao.sh
+++ dabao.sh
@@ -4,8 +4,8 @@ basepath=$(cd `dirname $0`; pwd)
 source ${basepath}/envirnment.sh
 
 # 打包
-#./gradlew clean
-#./gradlew assembleRelease
+./gradlew clean
+./gradlew assembleRelease
 
 # 删除原有文件并拷贝到临时目录
 rm -f ${TEMP}/*
